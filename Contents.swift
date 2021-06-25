93068f91430ebda05cfdefe199301d07d97e44fc
dada4747 <adsurerahul96@gmail.com>
2021-06-25 10:11:42 +0530

    Welcome to the EmployeeWageComputation in SWIFT Programming

---
+++ Contents.swift
@@ -0,0 +1,9 @@
+import Foundation
+let IS_PRESENT : Int = 1
+var empCheck = Int.random(in: 0...2)
+if empCheck == IS_PRESENT {
+    print("Employee is present")
+    
+}else {
+    print("Employee is Absent")
+}
