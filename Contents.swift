bcddc0176ea76f255190d3b5b0b42d6163c8fcc4
dada4747 <adsurerahul96@gmail.com>
2021-06-25 11:47:24 +0530

    ADDED:Functionality to check total employee wage for month

--- Contents.swift
+++ Contents.swift
@@ -2,18 +2,24 @@ import Foundation
 let IS_PART_TIME : Int = 1
 let IS_FULL_TIME : Int = 2
 let WAGE_PER_HR : Int = 20
+let NUM_OF_WORKING_DAYS :Int = 20
+
 var empHrs : Int = 0
 var empWage : Int = 0
-var empCheck = Int.random(in: 0...2)
+var totalEmpWage :Int = 0
 
-switch empCheck {
-case IS_PART_TIME:
-    empHrs = 4
-case IS_FULL_TIME:
-    empHrs = 8
-default:
-    empHrs = 0
+for day in 1...NUM_OF_WORKING_DAYS {
+    var empCheck = Int.random(in: 0...2)
+    switch empCheck {
+        case IS_PART_TIME:
+            empHrs = 4
+        case IS_FULL_TIME:
+            empHrs = 8
+        default:
+            empHrs = 0
+    }
+    empWage = WAGE_PER_HR * empHrs
+    totalEmpWage += empWage;
+    print("Employee wage of Day : \(day) : \(empWage) ")
 }
-
-empWage = WAGE_PER_HR * empHrs
-print("Employee Wage is : \(empWage)")
+print("Total Employee Wage is : \(totalEmpWage) ")
