b996a20a50f1d193de0225ae89786831bbe2eb03
dada4747 <adsurerahul96@gmail.com>
2021-06-25 12:19:57 +0530

    ADDED: Functionality to check employee wage per hr

--- Contents.swift
+++ Contents.swift
@@ -3,12 +3,16 @@ let IS_PART_TIME : Int = 1
 let IS_FULL_TIME : Int = 2
 let WAGE_PER_HR : Int = 20
 let NUM_OF_WORKING_DAYS :Int = 20
+let MAX_HRS_IN_MONTH :Int = 100
 
 var empHrs : Int = 0
 var empWage : Int = 0
 var totalEmpWage :Int = 0
+var totalEmpHrs :Int  = 0
+var totalWorkingDays : Int = 0
 
-for day in 1...NUM_OF_WORKING_DAYS {
+while totalEmpHrs <= MAX_HRS_IN_MONTH || totalWorkingDays < NUM_OF_WORKING_DAYS {
+    totalWorkingDays+=1
     var empCheck = Int.random(in: 0...2)
     switch empCheck {
         case IS_PART_TIME:
@@ -18,8 +22,10 @@ for day in 1...NUM_OF_WORKING_DAYS {
         default:
             empHrs = 0
     }
-    empWage = WAGE_PER_HR * empHrs
-    totalEmpWage += empWage;
-    print("Employee wage of Day : \(day) : \(empWage) ")
+    totalEmpHrs += empHrs
+    print(" Days : \(totalWorkingDays) Employee Hrs is  \(empHrs) ")
 }
+totalEmpWage = totalEmpHrs * WAGE_PER_HR;
+print("Total Employee hrs is : \(totalEmpHrs)")
 print("Total Employee Wage is : \(totalEmpWage) ")
+
