80c255899b4e792b77454e00b0e7f10bef496cd0
dada4747 <adsurerahul96@gmail.com>
2021-06-25 10:25:41 +0530

    ADDED:Functionality to check full time employee wage

--- Contents.swift
+++ Contents.swift
@@ -1,9 +1,14 @@
 import Foundation
 let IS_PRESENT : Int = 1
+let WAGE_PER_HR : Int = 20
+var empHrs : Int = 0
+var empWage : Int = 0
 var empCheck = Int.random(in: 0...2)
 if empCheck == IS_PRESENT {
-    print("Employee is present")
+   empHrs = 8
     
 }else {
-    print("Employee is Absent")
+    empHrs = 0
 }
+empWage = WAGE_PER_HR * empHrs
+print("Employee Wage is : \(empWage)")
