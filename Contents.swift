8aa0f209f1a508175fc22ded9235cc9536487cb4
dada4747 <adsurerahul96@gmail.com>
2021-06-25 10:36:14 +0530

    ADDED:Functionality to check Part time employee wage

--- Contents.swift
+++ Contents.swift
@@ -1,12 +1,15 @@
 import Foundation
-let IS_PRESENT : Int = 1
+let IS_PART_TIME : Int = 1
+let IS_FULL_TIME : Int = 2
 let WAGE_PER_HR : Int = 20
 var empHrs : Int = 0
 var empWage : Int = 0
 var empCheck = Int.random(in: 0...2)
-if empCheck == IS_PRESENT {
-   empHrs = 8
-    
+
+if empCheck == IS_PART_TIME {
+   empHrs = 4    
+}else if empCheck == IS_FULL_TIME {
+    empHrs = 8
 }else {
     empHrs = 0
 }
